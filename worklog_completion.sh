6855b09e2868be63338ad7e94c1ece54ebeb47a3
Francis <codepanda@gmail.com>
2015-03-22 10:24:44 -0400

    refactor bash completion function: group similar commands together for great DRY, -o nospace doesn't do anything here, determine available competion options on the command instead of the previous token

--- worklog_completion.sh
+++ worklog_completion.sh
@@ -1,27 +1,20 @@
 
 _worklog(){
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    local prev=${COMP_WORDS[COMP_CWORD-1]}
-    case $prev in
-        start)
-			COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
-            return 0
-            ;;
-        stop)
-            COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
-            return 0
-            ;;
-        resume)
-            COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
-            ;;
-        report)
-            return 0
-            ;;
-		--day)
-			return 0
+	local options
+
+	case "${COMP_WORDS[1]}" in
+		start|stop|resume)
+			options="--ago --at --day"
 			;;
-    esac
-    COMPREPLY=($(compgen -W "start stop resume report --day" -- $cur))
+		report)
+			options="--day"
+			;;
+		*)
+			options="start stop resume report"
+			;;
+	esac
+
+	COMPREPLY=( $( compgen -W "${options}" -- "${COMP_WORDS[COMP_CWORD]}" ) )
 }
 
 complete -F _worklog worklog
