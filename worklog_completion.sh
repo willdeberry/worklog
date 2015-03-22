2b378e6846e93ec8c5ad3aee7a550e1e9957fceb
haxwithaxe <me@haxwithaxe.net>
2015-03-21 23:28:10 -0400

    bash completion works as expected now

--- worklog_completion.sh
+++ worklog_completion.sh
@@ -1,28 +1,28 @@
 
-_comp_opts() {
-	$1
-}
-
-_worklog() {
-	local cur=${COMP_WORDS[COMP_CWORD]}
-	local prev=${COMP_WORDS[COMP_CWORD-1]}
-	case $prev in 
-		start)
-			COMPREPLY=($(compgen -W "--ago --at") -- $cur)
+_worklog(){
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+    case $prev in
+        start)
+			COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
+            return 0
+            ;;
+        stop)
+            COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
+            return 0
+            ;;
+        resume)
+            COMPREPLY=($(compgen -o nospace -W "--ago --at" -- $cur))
+            ;;
+        report)
+            return 0
+            ;;
+		--day)
 			return 0
 			;;
-		stop)
-			COMPREPLY=($(compgen -W "--ago --at") -- $cur)
-			return 0
-			;;
-		resume)
-			COMPREPLY=($(compgen -W "--ago --at") -- $cur)
-			;;
-		report)
-			return 0
-			;;
-	esac
-	COMPREPLY=($(compgen -W "start stop resume report --day" -- $cur))
+    esac
+    COMPREPLY=($(compgen -W "start stop resume report --day" -- $cur))
 }
 
 complete -F _worklog worklog
+
